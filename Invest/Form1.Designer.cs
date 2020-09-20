e0b49bab0c2377acd932976c0fa28711062c2d1b
Teetouch <47380304+saintnaja12@users.noreply.github.com>
2020-09-20 15:09:03 +0700

    Add files via upload

---
+++ Invest/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Invest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_money = new System.Windows.Forms.TextBox();
+            this.btn_sch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_month = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_out = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txt_money
+            // 
+            this.txt_money.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt_money.Location = new System.Drawing.Point(99, 419);
+            this.txt_money.Name = "txt_money";
+            this.txt_money.Size = new System.Drawing.Size(178, 22);
+            this.txt_money.TabIndex = 0;
+            this.txt_money.Enter += new System.EventHandler(this.txt_money_Enter);
+            this.txt_money.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_money_KeyPress);
+            this.txt_money.Leave += new System.EventHandler(this.txt_money_Leave);
+            // 
+            // btn_sch
+            // 
+            this.btn_sch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_sch.Location = new System.Drawing.Point(1001, 419);
+            this.btn_sch.Name = "btn_sch";
+            this.btn_sch.Size = new System.Drawing.Size(75, 23);
+            this.btn_sch.TabIndex = 2;
+            this.btn_sch.Text = "&Search";
+            this.btn_sch.UseVisualStyleBackColor = true;
+            this.btn_sch.Click += new System.EventHandler(this.btn_sch_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Start Money";
+            // 
+            // txt_month
+            // 
+            this.txt_month.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt_month.Location = new System.Drawing.Point(437, 419);
+            this.txt_month.Name = "txt_month";
+            this.txt_month.Size = new System.Drawing.Size(178, 22);
+            this.txt_month.TabIndex = 1;
+            this.txt_month.Enter += new System.EventHandler(this.txt_month_Enter);
+            this.txt_month.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_month_KeyPress);
+            this.txt_month.Leave += new System.EventHandler(this.txt_month_Leave);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 422);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Invest/Month";
+            // 
+            // lb_out
+            // 
+            this.lb_out.FormattingEnabled = true;
+            this.lb_out.ItemHeight = 16;
+            this.lb_out.Location = new System.Drawing.Point(515, 12);
+            this.lb_out.Name = "lb_out";
+            this.lb_out.Size = new System.Drawing.Size(561, 388);
+            this.lb_out.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 450);
+            this.Controls.Add(this.lb_out);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_month);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_sch);
+            this.Controls.Add(this.txt_money);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_money;
+        private System.Windows.Forms.Button btn_sch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_month;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lb_out;
+    }
+}
+
---
+++ Invest/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Invest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_sch_Click(object sender, EventArgs e)
+        {
+            Int32 year = 1;
+            Int32[] startmoney = new Int32[21];
+            Int32[] profit_bath = new Int32[21];
+            Int32 profit_per = 1;
+            Int32[] endyearmoney = new Int32[21];
+            int countArr = 0;
+            String strOutput = "";
+            Int32 inputStartMoney = 0;
+            int checkerr = 0;
+
+            try
+            {
+                inputStartMoney = Convert.ToInt32(txt_money.Text);
+            }
+            catch (Exception)
+            {
+                checkerr = -1;
+                MessageBox.Show("You must be enter right data , Error !!!");
+            }
+
+            if(checkerr == 0)
+            {
+                txtOutput.Text = "";
+                strOutput = "";
+
+                //for (year = 1; year <= 100; year++)
+                //{
+                //    textBox1.Text += "Year : " + year.ToString() + "\r\n";
+                //}
+
+                year = 1;
+                while (year <= 10)
+                {
+                    profit_per = 5;
+                    countArr = 0;
+                    while (profit_per <= 50)
+                    {
+                        strOutput += "Year : " + year.ToString("N0");
+
+                        if (year == 1)
+                        {
+                            startmoney[countArr] = inputStartMoney;
+                        }
+
+                        profit_bath[countArr] = (Int32)((decimal)(startmoney[countArr]) * (decimal)(profit_per) / (decimal)(100));
+                        endyearmoney[countArr] = startmoney[countArr] + profit_bath[countArr];
+
+                        strOutput += "[" +
+                           " : startMonery = " + startmoney[countArr].ToString("N0") +
+                           " : profit = " + profit_per.ToString("N0") + "%" + " (" + profit_bath[countArr].ToString("N0") + ") " +
+                           " : endofMoney = " + endyearmoney[countArr].ToString("N0") + "]";
+
+                        startmoney[countArr] = endyearmoney[countArr];
+                        profit_per += 5;
+                        countArr++;
+                    }
+                    strOutput += "\r\n";
+
+                    year++;
+                }
+                txtOutput.Text = strOutput;
+            }
+        }
+
+        private void txt_money_Enter(object sender, EventArgs e)
+        {
+            txt_money.SelectAll();
+        }
+
+        private void txt_month_Enter(object sender, EventArgs e)
+        {
+            txt_month.SelectAll();
+        }
+
+        private void txt_money_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == '\r')
+            {
+                e.Handled = true;
+                txt_month.Focus();
+            }
+
+            if ((e.KeyChar < '0' || e.KeyChar > '9') && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_month_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                e.Handled = true;
+                txt_money.Focus();
+            }
+            if ((e.KeyChar < '0' || e.KeyChar > '9') && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_money_Leave(object sender, EventArgs e)
+        {
+            Int32 input_money = 0; 
+            try
+            {
+                input_money = Convert.ToInt32(txt_money.Text);
+            }
+            catch(Exception)
+            {
+                txt_money.Text = "";
+            }
+        }
+
+        private void txt_month_Leave(object sender, EventArgs e)
+        {
+            Int32 input_month = 0;
+            try
+            {
+                input_month = Convert.ToInt32(txt_month.Text);
+            }
+            catch (Exception)
+            {
+                txt_month.Text = "";
+            }
+        }
+    }
+}
